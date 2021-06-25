70a8f3ecc0d1635adb0a68cd8df1f7e994b3ed85
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-06-24 23:46:19 -0300

    Rename 4) repetido.c to repetido.c

--- repetido.c
+++ repetido.c
@@ -6,27 +6,27 @@ int main ()
 	setlocale (LC_ALL, "Portuguese");
 	float num1, num2, num3;
 		
-	printf("--- Qual o número repetido? ---\n");
+	printf("--- Qual o nÃºmero repetido? ---\n");
 	printf("-----------------------------\n");
 	
-	printf("Digite o primeiro número: ");	
+	printf("Digite o primeiro nÃºmero: ");	
 	scanf("%f", &num1);
 	
-	printf("Digite o segundo número: ");	
+	printf("Digite o segundo nÃºmero: ");	
 	scanf("%f", &num2);
 	
-	printf("Digite o terceiro número: ");	
+	printf("Digite o terceiro nÃºmero: ");	
 	scanf("%f", &num3);
 	
 	if (num1 == num2 || num1 == num3) {
-		printf ("O número repetido é %.2f", num1);
+		printf ("O nÃºmero repetido Ã© %.2f", num1);
 	}
 		else {
 			if (num2 == num3) {
-				printf ("O número repetido é o %.2f", num2);
+				printf ("O nÃºmero repetido Ã© o %.2f", num2);
 			}
 			else {
-				printf ("Não há números repetidos");
+				printf ("NÃ£o hÃ¡ nÃºmeros repetidos");
 			}
 		}
 			
