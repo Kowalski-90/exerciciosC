2995d320a877d55de650fe8e28ec215f8fb1ea46
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-06-24 23:35:25 -0300

    Rename media.c to mediaescolar.c

--- mediaescolar.c
+++ mediaescolar.c
@@ -6,7 +6,7 @@ int main ()
 	setlocale (LC_ALL, "Portuguese");
 	float nota_p1, nota_p2, media;
 		
-	printf("--- Média semestral ---\n");
+	printf("--- MÃ©dia semestral ---\n");
 	printf("-----------------------------\n");
 	printf("Digite a nota da P1: ");	
 	scanf("%f", &nota_p1);
@@ -17,11 +17,11 @@ int main ()
 	
 	if (media >= 5)
 	{
-		printf("Parabéns! Você foi aprovado!");				
+		printf("ParabÃ©ns! VocÃª foi aprovado!");				
 	}
 	else 
 	{		
-	printf("Você foi reprovado! Faltaram %.2f pontos para atingir a média", (media-5)*-1);
+	printf("VocÃª foi reprovado! Faltaram %.2f pontos para atingir a mÃ©dia", (media-5)*-1);
 	}
 		
 	return 0;
