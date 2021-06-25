0fe9cf6c611c30b802e1359b07c1d465235cbc68
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-06-24 23:45:49 -0300

    Rename 1) posnegneutro.c to posnegneutro.c

--- posnegneutro.c
+++ posnegneutro.c
@@ -8,18 +8,18 @@ int main ()
 		
 	printf("--- Positivo, negativo ou nulo? ---\n");
 	printf("-----------------------------\n");
-	printf("Digite um n˙mero qualquer: ");	
+	printf("Digite um n√∫mero qualquer: ");	
 	scanf("%f", &numero);
 		
-	if (numero > 0)	{ //Se entra no IF, n„o entra no ELSE!
-		printf("O n˙mero digitado È positivo", numero);				
+	if (numero > 0)	{ //Se entra no IF, n√£o entra no ELSE!
+		printf("O n√∫mero digitado √© positivo", numero);				
 	}
 	else {
 		if (numero < 0) {
-			printf("O n˙mero digitado È negativo", numero);
+			printf("O n√∫mero digitado √© negativo", numero);
 		}
 		else {
-			printf("O n˙mero digitado È nulo", numero);
+			printf("O n√∫mero digitado √© nulo", numero);
 		}
 	}				
 	return 0;
