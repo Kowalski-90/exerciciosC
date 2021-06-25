214c103232aa8a77c90fe14391b76f62a19c7b10
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-06-24 23:35:41 -0300

    Rename 5) maior.c to maiorde2.c

--- maiorde2.c
+++ maiorde2.c
@@ -6,20 +6,20 @@ int main ()
 	setlocale (LC_ALL, "Portuguese");
 	float num1, num2;
 		
-	printf("--- Qual é o maior? ---\n");
+	printf("--- Qual Ã© o maior? ---\n");
 	printf("-----------------------------\n");
-	printf("Digite o primeiro número: ");	
+	printf("Digite o primeiro nÃºmero: ");	
 	scanf("%f", &num1);
-	printf("Digite o segundo número: ");
+	printf("Digite o segundo nÃºmero: ");
 	scanf("%f", &num2);
 	
 	if (num1 > num2)
 	{
-		printf("O maior número é: %.1f ", num1);				
+		printf("O maior nÃºmero Ã©: %.1f ", num1);				
 	}
 	else 
 	{		
-	printf("O maior número é: %.1f", num2);
+	printf("O maior nÃºmero Ã©: %.1f", num2);
 	}
 		
 	return 0;
