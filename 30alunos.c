30e84774617ff9493a57c9089e8908701c0504ab
Frederico Bonassa <61356476+Kowalski-90@users.noreply.github.com>
2021-06-26 07:59:45 -0300

    Rename 2) 30alunos.c to 30alunos.c

--- 30alunos.c
+++ 30alunos.c
@@ -5,27 +5,27 @@ int main () {
 	
 	setlocale (LC_ALL, "Portuguese");
 	
-	printf("--- Médias da turma de Algoritmos ---\n");
+	printf("--- MÃ©dias da turma de Algoritmos ---\n");
 	printf("-----------------------------------\n");
 	float media_t, media_l, media_f;
 	int i;
 	
 	for (i = 1; i <=30; i++) {
-		printf("\nInsira a média de teoria do aluno %i: ", i);
+		printf("\nInsira a mÃ©dia de teoria do aluno %i: ", i);
 		scanf("%f", &media_t);
-		printf("Insira a média de laboratório do aluno %i: ", i);
+		printf("Insira a mÃ©dia de laboratÃ³rio do aluno %i: ", i);
 		scanf("%f", &media_l);
 		media_f = media_t * 0.6 + media_l * 0.4;
-		printf("\nA média final do aluno %i foi de %.2f ", i, media_f);
+		printf("\nA mÃ©dia final do aluno %i foi de %.2f ", i, media_f);
 		if(media_f >=7){
-			printf("\nA média final foi boa");				
+			printf("\nA mÃ©dia final foi boa");				
 		}
 			else {
 				if(media_f < 5) {
-					printf("\nA média final foi ruim");
+					printf("\nA mÃ©dia final foi ruim");
 				}
 					else {
-						printf("\nA média final foi razoável");
+						printf("\nA mÃ©dia final foi razoÃ¡vel");
 					}
 			}			
 	}	
